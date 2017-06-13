dc5ec94f84e8dcca370bd44327c4787a1eca4e5e
Matt Davies <matt@agencycore.co.uk>
2017-06-13 14:00:19 +0100

    Final tweaks, homepage adjsutments and intro text size

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
-import ProjectControllerMixin from '../mixins/project-controller';
+import ProjectControllerMixin from '../mixins/project-controller-mixin';
 
 export default Ember.Controller.extend(ProjectControllerMixin, {});
