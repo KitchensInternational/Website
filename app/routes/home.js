62196caf4592114cfcd764d7a1b4277ec0a6c0dc
Matt Davies <matt@agencycore.co.uk>
2017-06-15 14:13:40 +0100

    Fixing IE assign object bug

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,13 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        let apiParams = { limit: 1, order: '-fields.publicationDate' },
-            articleApiParams = { 'fields.tag': 'news' },
-            eventApiParams = { 'fields.tag': 'event' };
         return Ember.RSVP.hash({
             content: this.get('store').queryRecord( 'homePage', { 'fields.slug': 'home' } ),
-            leadArticle: this.get('store').queryRecord( 'article', Object.assign(articleApiParams, apiParams) ),
-            leadEvent: this.get('store').queryRecord( 'article', Object.assign(eventApiParams, apiParams) )
+            leadArticle: this.get('store').queryRecord( 'article', { limit: 1, order: '-fields.publicationDate', 'fields.tag': 'news' } ),
+            leadEvent: this.get('store').queryRecord( 'article', { limit: 1, order: '-fields.publicationDate', 'fields.tag': 'event' } )
         });
     }
 });
