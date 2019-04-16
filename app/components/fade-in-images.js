917732b6e9fe696e6b121f4bfd4b6351c0f17268
Marko Rajevic <me@markorajevic.space>
2019-04-16 21:25:24 +0200

    fixes

--- app/components/fade-in-images.js
+++ app/components/fade-in-images.js
@@ -6,7 +6,7 @@ const SLIDE_PAUSE = 1800;
 
 function fadeInElement(elements, index) {
     Ember.$(elements[index]).delay(SLIDE_DELAY).fadeIn(FADE_IN_SPEED, function () {
-        if ( index < elements.length - 1 ) {
+        if (index < elements.length - 1) {
             Ember.$(this).delay(SLIDE_PAUSE).fadeOut(FADE_IN_SPEED, function () {
                 index = index + 1;
                 fadeInElement(elements, index);
@@ -18,7 +18,8 @@ function fadeInElement(elements, index) {
 export default Ember.Component.extend({
     classNames: ['fade-in-images'],
     didInsertElement() {
-        let elements = this.$('*');
+        let elements = this.$('*').slice(0, 2);
+        console.log('elements', elements);
         fadeInElement(elements, 0);
     }
 });
